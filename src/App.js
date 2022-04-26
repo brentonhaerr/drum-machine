9a2bb92c331b0743dd20b899ad7f46153592f09c
Brenton Haerr <brenton.haerr@gmail.com>
2022-04-26 13:04:05 -0700

    Added sound display element.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import DrumKeyboard from "./components/DrumKeyboard";
 import DrumMap from "./components/DrumMap";
+import SoundFXDisplay from "./components/SoundFXDisplay";
 
 function App() {
   return (
-    <div className="App">
+    <div id="drum-machine" className="App">
       <DrumMap />
+      <SoundFXDisplay />
       <DrumKeyboard />
     </div>
   );
---
+++ src/components/SoundFXDisplay.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SoundFXDisplay = () => {
+  return ( 
+    <div id="display">Sound Effect Here</div>
+   );
+}
+ 
+export default SoundFXDisplay;
