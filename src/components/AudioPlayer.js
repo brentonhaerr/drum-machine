4751a0e147e3b517d336658e8876ee732442f485
Brenton Haerr <brenton.haerr@gmail.com>
2022-04-28 08:43:33 -0700

    Added audio clips to audioplayer component.

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,21 +1,40 @@
 import React, { useEffect } from 'react';
 import {Howl, Howler} from 'howler';
+import tomhigh1fx from '../audio/tom-high-1.mp3';
+import tomhigh2fx from '../audio/tom-high-2.mp3';
+import cymballight1fx from '../audio/cymbal-light-1.mp3';
+import cymballight2fx from '../audio/cymbal-light-2.mp3';
+import kickfx from '../audio/kick.mp3';
+import snarefx from '../audio/snare.mp3';
+import tommid1fx from '../audio/tom-mid-low-1.mp3';
+import tommid2fx from '../audio/tom-mid-low-2.mp3';
+
+const playTomMid = () =>{
+  let sound = new Audio(kick);
+  sound.play();
+}
 
 const AudioPlayer = () => {
-  const cymballight1 = new Audio('../assets/cymballight1.mp3');
-  let sound_file = new Howl({
-    src: ['audio/snare.mp3']
-  });
+
+  const tomhigh1 = new Audio(tomhigh1fx);
+  const tomhigh2 = new Audio(tomhigh2fx);
+  const cymballight1 = new Audio(cymballight1fx);
+  const cymballight2 = new Audio(cymballight2fx);
+  const kick = new Audio(kickfx);
+  const snare = new Audio(snarefx);
+  const tommid1 = new Audio(tommid1fx);
+  const tommid2 = new Audio(tommid2fx);
 
   return (
     <div>
-      <div onClick={() => { document.getElementById("cymbal1").play(); }}>
-        Press to play
-      </div>
-      <audio id="cymbal1" src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" type="audio/mpeg" ></audio>
-      <button className="btn btn-primary" onClick={() => { sound_file.play() }}>
-        Press to play using howler
-      </button>
+      <audio id="tomhigh1" src="{tomhigh1}" type="audio/mpeg"></audio>
+      <audio id="tomhigh2" src="{tomhigh2}" type="audio/mpeg"></audio>
+      <audio id="cymballight1" src="{cymballight1}" type="audio/mpeg"></audio>
+      <audio id="cymballight2" src="{cymballight2}" type="audio/mpeg"></audio>
+      <audio id="kick" src="{kick}" type="audio/mpeg"></audio>
+      <audio id="snare" src="{snare}" type="audio/mpeg"></audio>
+      <audio id="tommid1" src="{tommid1}" type="audio/mpeg"></audio>
+      <audio id="tommid2" src="{tommid2}" type="audio/mpeg"></audio>
     </div>
   );
 }
--- src/components/DrumMap.js
+++ src/components/DrumMap.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import playTomMid from './AudioPlayer';
 
 const DrumMap = () => {
   return ( 
